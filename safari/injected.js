a1c2f37c6853cecc23f5384deb7662ba6ec3ec9d
E.Azer Koçulu <azer@kodfabrik.com>
2017-05-27 22:40:14 -0700

    make safari support basic messaging

--- safari/injected.js
+++ safari/injected.js
@@ -1,5 +1,12 @@
 const config = require("../config")
 
+addEventListener("message", function (e) {
+  if (e.data.from !== 'kozmos-web') return;
+  if (e.data.content === 'ping') send(e.data.id, 'pong')
+  if (e.data.content === 'bookmarks') send (e.data.id, [])
+})
+
+
 if (window.location.host === config.host.replace(/^\w+:\/\//, '')) {
   sendToken()
   window.addEventListener("hashchange", sendToken, false);
@@ -11,4 +18,13 @@ function sendToken () {
 
 function url () {
   return window.location.href
+}
+
+function send (id, content, error) {
+  postMessage({
+    from: 'kozmos-extension',
+    id: id,
+    content: content,
+    error: error
+  }, config.host)
 }
